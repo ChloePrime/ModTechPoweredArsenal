457c45b1cd05c5f205d2a67cc329e117dfd8c3db
ChloePrime <chloe_von_einzbern@qq.com>
2024-09-23 15:30:08 +0800

    修复本模组的创造标签里拿出来的枪无法开火且没有子弹的bug

--- src/main/java/mod/chloeprime/modtechpoweredarsenal/MTPA.java
+++ src/main/java/mod/chloeprime/modtechpoweredarsenal/MTPA.java
@@ -1,7 +1,11 @@
 package mod.chloeprime.modtechpoweredarsenal;
 
+import com.tacz.guns.api.TimelessAPI;
 import com.tacz.guns.api.item.builder.AttachmentItemBuilder;
+import com.tacz.guns.api.item.builder.GunItemBuilder;
+import com.tacz.guns.resource.pojo.data.gun.Bolt;
 import mod.chloeprime.modtechpoweredarsenal.common.standard.util.IfModLoadIngredient;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -10,6 +14,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.registries.RegistryObject;
+
+import static net.minecraft.world.item.Items.BARRIER;
 
 public final class MTPA {
     public static final class Items {
@@ -30,6 +36,26 @@ public final class MTPA {
         if (event.getRegistryKey().equals(ForgeRegistries.Keys.RECIPE_SERIALIZERS)) {
             CraftingHelper.register(loc("if_mod_loaded"), IfModLoadIngredient.Serializer.INSTANCE);
         }
+    }
+
+    static ItemStack gun(String namespace, String path) {
+        var loc = new ResourceLocation(namespace, path);
+        return TimelessAPI.getCommonGunIndex(loc).map(index -> {
+            var mode = index.getGunData().getFireModeSet().get(0);
+            var builder = GunItemBuilder.create()
+                    .setId(loc)
+                    .setAmmoCount(index.getGunData().getAmmoAmount())
+                    .setFireMode(mode);
+            // 上膛
+            if (index.getGunData().getBolt() == Bolt.CLOSED_BOLT) {
+                builder.setAmmoInBarrel(true);
+            }
+            return builder.build();
+        }).orElseGet(() -> BARRIER.getDefaultInstance().setHoverName(Component.literal("Unknown Weapon")));
+    }
+
+    static ItemStack gun(String path) {
+        return gun(loc(path).getNamespace(), path);
     }
 
     static ItemStack attachment(String namespace, String path) {
