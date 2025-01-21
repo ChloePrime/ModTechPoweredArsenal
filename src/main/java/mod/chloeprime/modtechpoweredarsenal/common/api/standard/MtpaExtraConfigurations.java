10d82de3dd1de62d6c2d917276549cce9d69a02b
Chloe Prime <chloe_von_einzbern@qq.com>
2025-01-21 20:10:06 +0800

    将火控功能分离为独立模组

--- src/main/java/mod/chloeprime/modtechpoweredarsenal/common/api/standard/MtpaExtraConfigurations.java
+++
@@ -1,8 +0,0 @@
-package mod.chloeprime.modtechpoweredarsenal.common.api.standard;
-
-public class MtpaExtraConfigurations {
-    public static final MtpaExtraConfiguration AIM_ASSIST = new MtpaExtraConfiguration("aim_assist");
-
-    private MtpaExtraConfigurations() {
-    }
-}
