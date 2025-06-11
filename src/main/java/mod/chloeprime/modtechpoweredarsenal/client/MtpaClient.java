e1452eb676b3ebe57a77b08d6a0b1c96cef2bc23
Chloe Prime <chloe_von_einzbern@qq.com>
2025-06-11 08:59:50 +0800

    清理imports

--- src/main/java/mod/chloeprime/modtechpoweredarsenal/client/MtpaClient.java
+++ src/main/java/mod/chloeprime/modtechpoweredarsenal/client/MtpaClient.java
@@ -11,9 +11,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.util.Random;
 import java.util.random.RandomGenerator;
