a2ac69cfc64f24c8a15fd96f5d2fdb1888ee6147
Chloe Prime <chloe_von_einzbern@qq.com>
2025-06-13 17:32:02 +0800

    增强启用法术注册表tag的mixin的兼容性

--- src/main/java/mod/chloeprime/modtechpoweredarsenal/mixin/iron_spell/MixinSpellRegistry.java
+++ src/main/java/mod/chloeprime/modtechpoweredarsenal/mixin/iron_spell/MixinSpellRegistry.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import net.minecraftforge.registries.RegistryBuilder;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -12,8 +13,9 @@ public class MixinSpellRegistry {
     /**
      * 让 Spell Registry 开启 tag 支持
      */
+    @Dynamic
     @ModifyExpressionValue(
-            method = "lambda$static$0",
+            method = "*",
             at = @At(value = "NEW", target = "()Lnet/minecraftforge/registries/RegistryBuilder;"))
     private static RegistryBuilder<AbstractSpell> enableTagSupport(RegistryBuilder<AbstractSpell> original) {
         return original.hasTags();
